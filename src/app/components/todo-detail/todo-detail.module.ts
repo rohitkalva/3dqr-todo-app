e45b4253d9a6e86e344b375f25ba35bc5c9e42d3
Rohit Kalva <rohitkalva@gmail.com>
2021-01-18 14:55:18 +0000

    Update todo-detail.module.ts

--- src/app/components/todo-detail/todo-detail.module.ts
+++ src/app/components/todo-detail/todo-detail.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 import { TodoDetailRoutingModule } from './todo-detail-routing.module';
 import { TodoDetailComponent } from './todo-detail.component';
 import { TodoService } from '../../services/todo.service';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-
-
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 @NgModule({
   declarations: [TodoDetailComponent],
   imports: [
@@ -16,7 +15,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     TodoDetailRoutingModule,
     MatFormFieldModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule
   ],
   providers: [
     TodoService
