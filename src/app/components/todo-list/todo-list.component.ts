712d8922aaa05c748b3eac45636f9b6b7ca64471
Rohit Kalva <rohitkalva@gmail.com>
2021-01-21 12:10:52 +0100

    CSS styling fix

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -55,7 +55,10 @@ export class TodoListComponent implements OnInit {
   getRandomColor() {
     var color = Math.floor(0x1000000 * Math.random()).toString(16);
     return '#' + ('000000' + color).slice(-6);
+
   }
+
+
 
 }
 
